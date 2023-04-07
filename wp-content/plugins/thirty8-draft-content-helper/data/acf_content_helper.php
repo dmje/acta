3b2cb045898156400e67bd565b1e035e87e2b8c6
Mike <mike@thirty8.co.uk>
2023-04-07 11:35:12 +0100

    Tweak to content helper

--- wp-content/plugins/thirty8-draft-content-helper/data/acf_content_helper.php
+++ wp-content/plugins/thirty8-draft-content-helper/data/acf_content_helper.php
@@ -108,7 +108,7 @@ acf_add_local_field_group(array(
 		),
 	),
 	'menu_order' => 0,
-	'position' => 'side',
+	'position' => 'acf_after_title',
 	'style' => 'default',
 	'label_placement' => 'top',
 	'instruction_placement' => 'label',
