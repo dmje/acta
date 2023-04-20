887997b00f99d5abe0daef2f238a254c051fbda4
Mike <mike@thirty8.co.uk>
2023-04-20 17:41:07 +0100

    Moving fonts and styles into child theme

---
+++ wp-content/themes/acta-bristol/functions.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// Add style.css, checking for cache
+function thirty8_enqueue_mainstylesheet(){
+
+	$main_css_ver = date("ymd-Gis", filemtime( get_stylesheet_directory() .'/css/style.css' ));
+    wp_enqueue_style( 'thirty8-style', get_stylesheet_directory_uri() . '/css/style.css',false, $main_css_ver );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'thirty8_enqueue_mainstylesheet' );
+
+
+
+
+?>
