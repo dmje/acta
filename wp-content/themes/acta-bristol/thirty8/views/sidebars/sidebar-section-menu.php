281209b9caedd77a25e025efa5d29980c06958ef
Adam Wilson <adam@aye-creative.co.uk>
2023-05-09 17:00:19 +0100

    Testing override

--- wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
+++ wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
@@ -3,20 +3,20 @@
 <aside class="widget inner-padding widget_pages">
 
 	<h2 class="widget-title">In this section</h2>
+
+	This is just a tribute
 		
 	<ul class="widget-menu">
 		<?php $parent = get_sub_field('parent_item'); ?>
-		<li><a href="<?php the_permalink($parent); ?>"><?php echo get_the_title(
-	$parent
-); ?></a></li>
+		<li><a href="<?php the_permalink($parent); ?>"><?php echo get_the_title($parent); ?></a></li>
 		<ul class="children">
-		<?php
-  $args = [
-  	'title_li' => '',
-  	'child_of' => $parent,
-  ];
-  wp_list_pages($args);
-  ?>
+			<?php
+				$args = [
+					'title_li' => '',
+					'child_of' => $parent,
+				];
+				wp_list_pages($args);
+			?>
 		</ul>
 	</ul>
 			
