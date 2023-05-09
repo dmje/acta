cc60555b663efebe2749878600682e9fcd68d74d
Mike <mike@thirty8.co.uk>
2023-05-09 18:13:06 +0100

    Tidying plugins

--- wp-content/plugins/elasticpress/dist/css/instant-results-styles.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '8acd22f6c3e3ceab6922');
--- wp-content/plugins/elasticpress/dist/js/facets-meta-block-script.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '332c5a9c049e4c89d150');
