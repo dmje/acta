5231c8ba22e96dfdee1bc8451520177e2c3ab560
Mike <mike@thirty8.co.uk>
2023-05-09 16:55:33 +0100

    Update sidebar-section-menu.php

--- wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
+++ wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
@@ -5,13 +5,7 @@
 	<h2 class="widget-title">In this section</h2>
 		
 	<ul class="widget-menu">
-		<?php // use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-// use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-// use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-  // use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-  // use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-  // use wp_list_pages to display parent and all child pages all generations (a tree with parent)
-  $parent = get_sub_field('parent_item'); ?>
+		<?php $parent = get_sub_field('parent_item'); ?>
 		<li><a href="<?php the_permalink($parent); ?>"><?php echo get_the_title(
 	$parent
 ); ?></a></li>
