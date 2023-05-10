300b68212916faf88204f517e56ce06b8d5f55c3
Adam Wilson <adam@aye-creative.co.uk>
2023-05-10 09:40:26 +0100

    Improved slide

--- wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
+++ wp-content/themes/acta-bristol/thirty8/views/sidebars/sidebar-section-menu.php
@@ -21,45 +21,20 @@
 				var closest_li = link.closest("li");
 				var link_status = closest_li.hasClass("active");
 				var count = 0;
+				var slideDuration = 400;
 
-				closest_ul.find("ul").slideUp(function() {
+				closest_ul.find("ul").stop(true, true).fadeOut({ duration: slideDuration, queue: false }).slideUp(slideDuration, function() {
 					if (++count == closest_ul.find("ul").length)
-							parallel_active_links.removeClass("active");
+						parallel_active_links.removeClass("active");
 				});
 
 				if (!link_status) {
-					closest_li.children("ul").slideDown();
+					closest_li.children("ul").stop(true, true).fadeIn({ duration: slideDuration, queue: false }).css('display', 'none').slideDown(slideDuration);
 					closest_li.addClass("active");
 				}
 			})
 		});
 	</script>
-
-	<style>
-		.widget-menu .page_item_has_children ul { display: none; }
-
-		.widget-menu .page_item_has_children.active > ul { display: block; }
-
-		.widget ul li.menu-item-has-children, .widget ul li.page_item_has_children {
-			padding-bottom: 5px !important;
-		}
-
-		.widget ul li.page_item_has_children > a:after {
-			content: "\f107";
-			font-family: GeneratePress;
-			display: inline-block;
-			width: 0.8em;
-			font-size: 18px;
-			text-align: left;
-			margin-left: 7px;
-			line-height: 19px;
-			vertical-align: middle;
-		}
-
-		.widget ul li.page_item_has_children.active > a:after {
-			transform: rotate(180deg);
-		}
-	</style>
 
 	<h2 class="widget-title">In this section</h2>
 		
